3c0458eb354fcfe8d19c1f32f9f004d1bcd0c15e
eric9530873 <eric9530873@gmail.com>
2023-07-31 00:09:48 +0800

    7/30

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@
 const fs = require('fs');
 
 const path = require('path');
-require('dotenv').config({ path: require('find-config')('.env') })
+require('dotenv').config()
 
 const Sequelize = require('sequelize');
 const process = require('process');
