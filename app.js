6ac878dfcff6fa718c6cbbb3dc9b87deda3dc6ff
eric9530873 <eric9530873@gmail.com>
2023-08-02 17:23:13 +0800

    8/2

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 3000
 
 const cors = require('cors')
 app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 }))
 
 const methodOverride = require('method-override')
