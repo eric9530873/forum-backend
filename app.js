4440f8f06301647ecd9a1538ce2d0a574c99af2c
eric9530873 <eric9530873@gmail.com>
2023-08-15 11:48:56 +0800

    8/15

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 3000
 const cors = require('cors')
 
 
-app.options('*', cors())
+app.options('*', cors({
+    "preflightContinue": true
+}))
 
 app.use(cors({
     origin: 'https://eric9530873.github.io/forum1231'
